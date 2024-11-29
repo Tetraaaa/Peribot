f13f6ac78acb06cb8d0d60583f2a902ce273699c
Tetra <nicolas.strohl@gmail.com>
2024-11-29 20:00:24 +0100

    [Fix] Le serveur express se shutdown correctement quand le process est tué

--- Tools/server.ts
+++ Tools/server.ts
@@ -1,11 +1,22 @@
 import CONFIG from "config";
 import express from "express";
 import { peribot } from "peribot";
+import process from "node:process";
+import logger from "./logger";
+
+function shutDownExpressServer(app) {
+  logger.info("Shutting down express server...");
+  app.close();
+}
 
 export function runExpressServer() {
   console.log("Démarrage du serveur express...");
 
   const app = express();
+
+  process.on("SIGTERM", () => shutDownExpressServer(app));
+  process.on("SIGINT", () => shutDownExpressServer(app));
+  process.on("SIGKILL", () => shutDownExpressServer(app));
 
   app.get("/", (_, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
