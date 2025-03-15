67b85734f1c0e4251e13cbfe5786ecc6e63b4ff7
Tetra <nicolas.strohl@gmail.com>
2025-03-15 10:46:12 +0100

    Amélioration du logger pour aussi afficher le log dans la console

--- Tools/logger.ts
+++ Tools/logger.ts
@@ -2,34 +2,29 @@ import fs from "node:fs";
 import pino, { multistream } from "pino";
 
 const streams = [
-  { level: "debug", stream: fs.createWriteStream("logs/debug.stream.out") },
-  { level: "error", stream: fs.createWriteStream("logs/error.stream.out") },
+  { stream: fs.createWriteStream("logs/peribot.log", { flags: "a" }) },
 ];
-
-var opts = {
-  levels: {
-    silent: Infinity,
-    fatal: 60,
-    error: 50,
-    warn: 50,
-    info: 30,
-    debug: 20,
-    trace: 10,
-  },
-  dedupe: true,
-};
 
 var logger = pino(
   {
-    level: "debug",
+    level: "info",
     formatters: {
       level: (label) => {
         return { level: `[${label.toLocaleUpperCase()}]` };
       },
+      bindings: () => {
+        return {};
+      },
+    },
+    hooks: {
+      logMethod(args, method) {
+        console.log(...args); //Also log to console to make debugging easier
+        method.apply(this, args);
+      },
     },
     timestamp: pino.stdTimeFunctions.isoTime,
   },
-  multistream(streams, opts)
+  multistream(streams)
 );
 
 export default logger;
